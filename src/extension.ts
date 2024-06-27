6e40d2ca468209295d81b1cb334f95f81c373b15
Nico-Mayer <nico.m0623@gmail.com>
2024-06-27 08:56:57 +0200

    removed esbuild

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('git-differ.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Git Differ!');
+		vscode.window.showInformationMessage('Hello World from Git differ!');
 	});
 
 	context.subscriptions.push(disposable);
