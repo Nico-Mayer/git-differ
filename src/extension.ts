e01936e78e583ae12487549bf7dc8b1176e10644
Nico-Mayer <nico.m0623@gmail.com>
2024-06-27 21:27:40 +0200

    readme

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,78 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'
+import * as vscode from "vscode";
+import { API, GitExtension, Ref } from "./git";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "git-differ" is now active!')
+  const gitExtension =
+    vscode.extensions.getExtension<GitExtension>("vscode.git")!.exports;
+  const gitApi: API = gitExtension.getAPI(1);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand(
-		'git-differ.helloWorld',
-		() => {
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
-			vscode.window.showInformationMessage('Hello World from Git differ!')
-		}
-	)
+  const compareWithBranch = vscode.commands.registerCommand(
+    "git-differ.compareWithBranch",
+    async (uri: vscode.Uri) => {
+      const branches = await getBranches(gitApi, uri, true);
+      const branchNames = branches
+        .map((branch) => branch.name)
+        .filter((name): name is string => !!name);
 
-	context.subscriptions.push(disposable)
+      const selectedBranchName = await vscode.window.showQuickPick(
+        branchNames,
+        {
+          placeHolder: "Select a branch to compare with",
+        }
+      );
+
+      if (!selectedBranchName) {
+        vscode.window.showErrorMessage("No branch selected");
+        return;
+      }
+
+      const branch = branches.find(
+        (branch) => branch.name === selectedBranchName
+      );
+      if (!branch) {
+        vscode.window.showErrorMessage(
+          `Branch not found: ${selectedBranchName}`
+        );
+        return;
+      }
+
+      if (branch.name === undefined) {
+        vscode.window.showErrorMessage(
+          `Branch Name is undefined: ${selectedBranchName}`
+        );
+        return;
+      }
+
+      const gitUri = gitApi.toGitUri(uri, branch.name);
+      const filePath = uri.path.split("/").pop() || uri.path;
+
+      vscode.commands.executeCommand(
+        "vscode.diff",
+        gitUri,
+        uri,
+        `${branch.name} compared with "${filePath}"`
+      );
+    }
+  );
+
+  context.subscriptions.push(compareWithBranch);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function getBranches(
+  gitApi: API,
+  uri: vscode.Uri,
+  remote: boolean
+): Promise<Ref[]> {
+  const repo = gitApi.getRepository(uri);
+
+  if (!repo) {
+    vscode.window.showErrorMessage(
+      `Repository not found for URI: ${uri.toString()}`
+    );
+    return [];
+  }
+
+  return await repo.getBranches({ remote });
+}
